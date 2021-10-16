e6c89591afcdd0c326fdd26ba22c409657393804
Ng Shen Jie <ng.shen.jie1999@gmail.com>
2021-10-16 20:26:22 +0800

    Update

--- app.py
+++ app.py
@@ -94,4 +94,4 @@ def index():
         return 'Invalid Email Id',400
    
 if __name__ == '__main__':
-   app.run(debug=False, host='0.0.0.0', port='5000')
+   app.run(debug=False, host='0.0.0.0', port='5000') 
